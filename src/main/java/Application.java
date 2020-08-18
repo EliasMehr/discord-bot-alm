9c5dc39b812f77a649261f4a4574c28dc3769833
eliasmehr <Falafel123!>
2020-08-18 21:28:09 +0200

    Code base completed and ready for development

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,9 +1,24 @@
+import Bot.DiscordBot;
+
+import javax.security.auth.login.LoginException;
+import static net.dv8tion.jda.api.OnlineStatus.ONLINE;
+import static net.dv8tion.jda.api.entities.Activity.watching;
+
 public class Application {
     public static void main(String[] args) {
-
+        try {
         // 1. Instantiate Discord bot configuration class
+            DiscordBot discordBot = new DiscordBot();
+
         // 2. Setup global configuration for Discord bot at startup
+            discordBot.getAPI().getPresence().setActivity(watching("Over my kids"));
+            discordBot.getAPI().getPresence().setStatus(ONLINE);
+
         // 3. Add event classes to Discord bot object ( All event classes need to extend ListenerAdapter )
+            // 3.1 Let's create events and add them to the bot :)
+        } catch (LoginException e) {
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/Bot/DiscordBot.java
+++ src/main/java/Bot/DiscordBot.java
@@ -1,18 +1,35 @@
 package Bot;
 
-import lombok.Getter;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 
-@Getter
+import javax.security.auth.login.LoginException;
+import java.util.EnumSet;
+
 public class DiscordBot {
-
     public static final String prefix = "$";
-    private final String token = "ADD_BOT_CLIENT_SECRET_KEY_HERE";
+    private final JDA API;
+    private final String TOKEN = "ADD_BOT_CLIENT_SECRET_KEY_HERE"; // Keep token secured, use System.getenv("$TOKEN_VARIABLE")
 
-    public DiscordBot() {
+    public DiscordBot() throws LoginException {
         // 1. Setup a EnumSet of GatewayIntent and load all permissions to list
+        EnumSet<GatewayIntent> gatewayIntents = initializeGateways();
         // 2. Build JDA Api configuration with api token and permission list
+        API = JDABuilder.create(TOKEN, gatewayIntents).build();
     }
 
     // 1. Create method with return type of EnumSet<GatewayIntent> with no params
+    private EnumSet<GatewayIntent> initializeGateways() {
+        return EnumSet.allOf(GatewayIntent.class);
+    }
+
     // 2. Create dynamic method that allows us to add eventListeners from application runtime
+    public void addEventListener(Object listener) {
+        API.addEventListener(listener);
+    }
+
+    public JDA getAPI() {
+        return API;
+    }
 }
