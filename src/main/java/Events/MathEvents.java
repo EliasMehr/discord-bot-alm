96d363368e79302b75d42223bab25bac51dcbc92
Jessie Eurenius <jessie930818@gmail.com>
2020-08-19 11:39:09 +0200

    Added functions to MathEvents

--- src/main/java/Events/MathEvents.java
+++ src/main/java/Events/MathEvents.java
@@ -1,4 +1,31 @@
 package Events;
 
-public class MathEvents {
+import Bot.DiscordBot;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+import javax.annotation.Nonnull;
+
+public class MathEvents extends ListenerAdapter {
+
+    @Override
+    public void onGuildMessageReceived(@Nonnull GuildMessageReceivedEvent event) {
+        String[] args = event.getMessage().getContentRaw().split("\\s+");
+        if(args[0].equalsIgnoreCase(DiscordBot.prefix + "calc")) {
+
+            if(args.length < 3) {
+
+            }
+        }
+    }
+
+    public boolean isNumber(String str) {
+        try {
+            double number = Double.parseDouble(str);
+            return true;
+        }
+        catch (NumberFormatException e) {
+            return false;
+        }
+    }
 }
