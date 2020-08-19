562b4543dc3923517cc5f4738e5e60879ae3ee76
eliasmehr <Falafel123!>
2020-08-19 11:06:26 +0200

    Added InvitationEvent class

--- src/main/java/Events/InvitationEvent.java
+++ src/main/java/Events/InvitationEvent.java
@@ -1,12 +1,11 @@
 package Events;
 
-import Bot.DiscordBot;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
-import static Bot.DiscordBot.*;
+import static Bot.DiscordBot.prefix;
 
 public class InvitationEvent extends ListenerAdapter {
 
@@ -19,6 +18,7 @@ public class InvitationEvent extends ListenerAdapter {
         if (args[0].equalsIgnoreCase(prefix + "invite")) {
             String invitationURL = event.getChannel().createInvite().complete().getUrl();
             String invitedBy = event.getAuthor().getName();
+            
             event.getChannel()
                     .sendMessage("Hey you!, " + invitedBy + " give your friend this friendly link to join us.. " + invitationURL)
                     .queue();
