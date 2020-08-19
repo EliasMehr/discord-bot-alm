c5917989262f45c5a72f21fae9da2c80f92b331e
eliasmehr <Falafel123!>
2020-08-19 12:50:30 +0200

    Fixed minor issues on master

--- src/main/java/Events/InvitationEvent.java
+++ src/main/java/Events/InvitationEvent.java
@@ -3,12 +3,7 @@ package Events;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
-
-
 import static Bot.DiscordBot.PREFIX;
-
-import static Bot.DiscordBot.prefix;
-
 
 public class InvitationEvent extends ListenerAdapter {
 
@@ -18,8 +13,6 @@ public class InvitationEvent extends ListenerAdapter {
 
 
         if (args[0].equalsIgnoreCase(PREFIX + "invite")) {
-
-        if (args[0].equalsIgnoreCase(prefix + "invite")) {
 
             String invitationURL = event.getChannel().createInvite().complete().getUrl();
             String invitedBy = event.getAuthor().getName();
